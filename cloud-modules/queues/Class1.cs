ab2f47ee02cdb5ce3be75b94a492898c5bfe80aa
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-05-17 16:50:29 -0500

    WeLearnedAsync

--- cloud-modules/queues/Class1.cs
+++ cloud-modules/queues/Class1.cs
@@ -1,5 +1,27 @@
-﻿namespace queues;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace queues;
 public class Class1
 {
 
+	public async Task<string> send(string msg)
+	{
+		Action<object> action = (object obj) => {
+			Console.WriteLine("Hola {0}", obj);
+		};
+
+		await Task.Factory.StartNew(action, "beta");
+		return "A message Identifier";
+	}
+
+
+	public async Task<string> receive(Action<string> onReceive)
+	{
+		string body = "this is the body";
+		await  Task.Run(() => onReceive.Invoke(body));
+
+		return "A message Identifier";
+	}
 }
