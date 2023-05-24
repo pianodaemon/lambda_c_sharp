be6aca74895f42ce9c39eb81ef2dbdf5424729dd
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-05-23 20:59:35 -0500

    testing_works

--- cloud-modules/queues.Tests/UnitTest1.cs
+++ cloud-modules/queues.Tests/UnitTest1.cs
@@ -3,8 +3,8 @@ namespace queues.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void should_send_receive_and_delete_flawlessly()
     {
-
+        Assert.True(true);
     }
 }
