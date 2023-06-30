fba0c1665cb62fca4d8024dabd73e61fcbc179cc
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-06-30 09:51:21 -0500

    it_works

--- cloud-modules/queues.Tests/BucketTests.cs
+++ cloud-modules/queues.Tests/BucketTests.cs
@@ -2,7 +2,7 @@ using Amazon;
 using Amazon.S3;
 using Amazon.Runtime;
 using Amazon.S3.Model;
-using Amazon.S3.Transfer;
+using System.IO;
 
 namespace CloudModules.Tests;
 
@@ -13,7 +13,7 @@ public class BucketTests
     private static readonly string AccessKey = "ignore";
     private static readonly string _testB = "test-bucket";
     private string _localstackServiceUrl;
-    private static AmazonS3Client obtainS3Client(string url) => new AmazonS3Client(new BasicAWSCredentials(AccessKey, SecretKey), new AmazonS3Config { ServiceURL = url });
+    private static AmazonS3Client obtainS3Client(string url) => new AmazonS3Client(new BasicAWSCredentials(AccessKey, SecretKey), new AmazonS3Config { ServiceURL = url, UseHttp = true, ForcePathStyle = true, AuthenticationRegion = "us-east-1" });
 
     public BucketTests(LocalstackContainer lsc)
     {
@@ -25,5 +25,22 @@ public class BucketTests
     {
         var client = BucketTests.obtainS3Client(_localstackServiceUrl);
         Assert.True(client is not null, "s3 client incorrectly set");
+        var b = new Bucket(_testB, client);
+        FileStream fs = new FileStream("/etc/services", FileMode.Open, FileAccess.Read);
+        b.upload("text/plain","services_copy.txt", fs);
+        using Stream streamToWriteTo = File.Open("/tmp/services_copy.txt", FileMode.Create);
+        var t0 = b.download("services_copy.txt");
+        t0.Wait();
+        CopyStream(t0.Result, streamToWriteTo);
+    }
+
+    private static void CopyStream(Stream input, Stream output)
+    {
+        byte[] buffer = new byte[1<<12];
+        int len;
+        while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            output.Write(buffer, 0, len);
+        }
     }
 }
