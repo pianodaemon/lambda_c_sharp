0dddb2bd9dfc03e8aa1a6d4c0c6f7bc8d0dc3d41
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-07-04 16:43:04 -0500

    transconsumer

---
+++ cloud-modules/queues/TransConsumer.cs
@@ -0,0 +1,40 @@
+namespace CloudModules;
+
+public class TransConsumer
+{
+    public static async Task<List<R>> DoConsume<T, R>(Func<T, R> transHandler, ICloudQueue<T> q)
+    {
+        var productionList = new List<R>();
+        Action<T> actOnReceiveHandler = (tpo) =>
+        {
+            R elementTransformed = transHandler(tpo);
+            if (elementTransformed != null) productionList.Add(elementTransformed);
+        };
+
+        short consumptionCounter = 0;
+        try
+        {
+            for(;;consumptionCounter++)
+            {
+                await q.receiveJsonAsObject(actOnReceiveHandler);
+            }
+        }
+        catch (AggregateException ae)
+        {
+            ae.Handle((ex) =>
+            {
+                // This is what we expect to handle as
+		// the minimal consumption.
+                if ((ex is CloudModuleException) && (consumptionCounter > 0) &&
+                   (((CloudModuleException) ex).ErrorCode == ErrorCodes.NO_MESSAGES_FOUND_IN_QUEUE))
+                {
+                    return true;
+                }
+
+                return false;
+            });
+        }
+
+        return productionList;
+    }
+}
