9c8f090d9843aeb76d774478fc6d6f5cc0731c4b
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-07-13 15:15:08 -0500

    test

--- cloud-modules/queues.Tests/localstack/provisioning/scripts/test-transconsumer.sh
+++ cloud-modules/queues.Tests/localstack/provisioning/scripts/test-transconsumer.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+TEST_TCB="s3://test-transconsumer-bucket"
+awslocal s3 mb $TEST_TCB
+echo "A S3 bucket ${TEST_TCB} has been provisioned"
+
 TEST_TCQ="test-transconsumer-queue"
 awslocal sqs create-queue --queue-name $TEST_TCQ
-
 echo "A SQS queue ${TEST_TCQ} has been provisioned"
