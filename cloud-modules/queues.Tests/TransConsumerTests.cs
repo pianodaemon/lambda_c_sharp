3a1a8eb385d153887c51c2213e67dde81ad3b14f
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-07-13 15:17:42 -0500

    dsd

--- cloud-modules/queues.Tests/TransConsumerTests.cs
+++ cloud-modules/queues.Tests/TransConsumerTests.cs
@@ -4,6 +4,7 @@ using Amazon.SQS.Model;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
+using System.IO;
 
 namespace CloudModules.Tests;
 
@@ -12,8 +13,10 @@ public class TransConsumerTests
 {
     private static readonly string SecretKey = "ignore";
     private static readonly string AccessKey = "ignore";
+    private static readonly string _testB = "test-transconsumer-bucket";
     private static readonly string _testQ = "test-transconsumer-queue";
     private string _localstackServiceUrl;
+    private static AmazonS3Client obtainS3Client(string url) => new AmazonS3Client(new BasicAWSCredentials(AccessKey, SecretKey), new AmazonS3Config { ServiceURL = url, UseHttp = true, ForcePathStyle = true, AuthenticationRegion = "us-east-1" });
     private static AmazonSQSClient obtainSqsClient(string url) => new AmazonSQSClient(new BasicAWSCredentials(AccessKey, SecretKey), new AmazonSQSConfig { ServiceURL = url });
 
     public TransConsumerTests(LocalstackContainer lsc)
@@ -24,29 +27,47 @@ public class TransConsumerTests
     [Fact]
     public void should_transconsumeCorrectly()
     {
+        var b = new Bucket(_testB, obtainS3Client(_localstackServiceUrl));
+        FileStream fs = new FileStream("/etc/hosts", FileMode.Open, FileAccess.Read);
+        b.upload("text/plain","/etc/hosts_copy.txt", fs);
+
         var tz = isQueuePresent(obtainSqsClient(_localstackServiceUrl), _testQ);
         tz.Wait();
         Assert.True(tz.Result, $"Queue {_testQ} is not present");
 
-        Func<PlainObjectMock, ReturnMock> transHandler = (tpo) =>
+        Func<MetaMsg, ReturnMock?> transHandler = (tpo) =>
         {
             var rmo = new ReturnMock();
-            rmo.Text = tpo.Text;
+            rmo.Text = tpo.BucketObjKey;
+            using Stream streamToWriteTo = File.Open("/tmp/hosts_copy.txt", FileMode.Create);
+            var td = b.download("/etc/hosts_copy.txt");
+            td.Wait();
+            CopyStream(td.Result, streamToWriteTo);
             return rmo;
         };
 
-        ICloudQueue<PlainObjectMock> q = obtainSteadyQueue4Test<PlainObjectMock>(_localstackServiceUrl, _testQ);
+        ICloudQueue<MetaMsg> q = obtainSteadyQueue4Test<MetaMsg>(_localstackServiceUrl, _testQ);
 
         // Let us push a json message
-        var obj = new PlainObjectMock();
+        var obj = new MetaMsg();
         {
-            obj.Text = "Be here now";
-            var t0 = q.SendObjectAsJson(obj);
+            obj.BucketObjKey = "/etc/hosts_copy.txt";
+            var t0 = q.sendObjectAsJson(obj);
             t0.Wait();
         }
 
-        var list = TransConsumer.DoConsume<PlainObjectMock, ReturnMock>(transHandler, q);
-        Assert.True(obj.Text == list.First().Text, "Unexpected text as result");
+        var list = TransConsumer.DoConsume<MetaMsg, ReturnMock>(transHandler, q);
+        Assert.True(obj.BucketObjKey == list.First().Text, "Unexpected text as result");
+    }
+
+    private static void CopyStream(Stream input, Stream output)
+    {
+        byte[] buffer = new byte[1<<12];
+        int len;
+        while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            output.Write(buffer, 0, len);
+        }
     }
 
     private static JsonifiedQueue<T> obtainSteadyQueue4Test<T>(string lss, string queueName)
@@ -70,25 +91,25 @@ public class TransConsumerTests
     }
 }
 
-public class PlainObjectMock
+public class MetaMsg
 {
-    public string? Text { get; set; }
+    public string? BucketObjKey { get; set; }
 
     public override bool Equals(object? obj)
     {
-        var item = obj as PlainObjectMock;
+        var item = obj as MetaMsg;
 
-        if (item == null || this.Text == null)
+        if (item == null || this.BucketObjKey == null)
         {
             return false;
         }
 
-        return this.Text.Equals(item.Text);
+        return this.BucketObjKey.Equals(item.BucketObjKey);
     }
 
     public override int GetHashCode()
     {
-        return this.Text != null ? this.Text.GetHashCode() : 0;
+        return this.BucketObjKey != null ? this.BucketObjKey.GetHashCode() : 0;
     }
 }
 
