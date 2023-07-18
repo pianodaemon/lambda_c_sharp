727acb8bcd5044e3537bff928ca458b7cc875efc
Edwin Pĺauchu <pianodaemon@gmail.com>
2023-07-18 11:05:50 -0500

    sm

---
+++ sm.pl
@@ -0,0 +1,41 @@
+use strict;
+
+sub _wrap_execution {
+    my $in_stream = shift;
+    my %kvargs = @_;
+    my $buff_str;
+    {
+        my $pid = open my $out_stream, "|-", "$kvargs{consumer}", "$kvargs{bridge}";
+        open my $STDOLD, '>&', STDOUT;
+        open STDOUT, '>', \$buff_str;     # Pretending a string is the stdout file
+        while( <$in_stream> ) {
+            print $out_stream "$_";       # It writes lines to the consumer application
+        }
+        open STDOUT, '>&', $STDOLD;
+        close $out_stream;
+    }
+
+    {
+        local $/;
+        return <$buff_str>;
+    }
+}
+
+use constant {
+    CONSUME_APP => "cloud-modules/consumer-app/bin/Debug/net6.0/consumer-app",
+};
+
+sub fetch_secret_str {
+    my $buff_str = shift;
+    open my $IN_DATA, "<", \$buff_str;
+    my $out_buffer = &_wrap_execution(
+        *$IN_DATA,
+        'consumer' => CONSUME_APP,
+        'bridge'   => "BRIDGE_SECRET_ID_REQ",
+    );
+    close $IN_DATA;
+    return $out_buffer;
+}
+
+
+print fetch_secret_str "sheldon-cooper-says"
