addfb4b552612dcea2fc4e07e67515a2b5c2a7ae
Edwin Plauchu <pianodaemon@gmail.com>
2024-06-15 12:51:48 -0600

    Create MessageHelper.cs

---
+++ bride-data/pocconsumer/MessageHelper.cs
@@ -0,0 +1,20 @@
+namespace POCConsumer;
+
+using System;
+using Newtonsoft.Json.Linq;
+
+public static class MessageHelper
+{
+    public const string FileKeyField = "fileKey";
+    public const string TargetPathField = "targetPath";
+
+    public static record struct BridgePartialData(string FileKey, string TargetPath);
+
+    public static BridgePartialData DecodeMessage(string messageBody)
+    {
+        JObject json = JObject.Parse(messageBody);
+        string fileKey = json[FileKeyField]?.ToString() ?? throw new ArgumentNullException(FileKeyField, $"{FileKeyField} is required and cannot be null or empty.");
+        string targetPath = json[TargetPathField]?.ToString() ?? throw new ArgumentNullException(TargetPathField, $"{TargetPathField} is required and cannot be null or empty.");
+        return new BridgePartialData(fileKey, targetPath);
+    }
+}
