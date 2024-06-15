65bd243bef2b174efd4167c0f5379a99f82ef8fe
Edwin Plauchu <j4nusx@yahoo.com>
2024-06-15 18:03:54 -0500

    xxx

--- bridge-data/pocconsumer/MessageHelper.cs
+++ bridge-data/pocconsumer/MessageHelper.cs
@@ -11,8 +11,9 @@ public static class MessageHelper
     public static BridgePartialData DecodeMessage(string messageBody)
     {
         JObject json = JObject.Parse(messageBody);
-        string fileKey = json[FileKeyField]?.ToString() ?? throw new ArgumentNullException(FileKeyField, $"{FileKeyField} is required and cannot be null or empty.");
-        string targetPath = json[TargetPathField]?.ToString() ?? throw new ArgumentNullException(TargetPathField, $"{TargetPathField} is required and cannot be null or empty.");
-        return new BridgePartialData(fileKey, targetPath);
+        return new BridgePartialData(
+            json[FileKeyField]?.ToString() ?? throw new ArgumentNullException(FileKeyField, $"{FileKeyField} is required and cannot be null or empty."),
+            json[TargetPathField]?.ToString() ?? throw new ArgumentNullException(TargetPathField, $"{TargetPathField} is required and cannot be null or empty.")
+        );
     }
 }
