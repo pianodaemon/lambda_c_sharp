1e920bc2a745c01eadda65220327cf24a7388fbc
Edwin Plauchu <j4nusx@yahoo.com>
2024-06-24 10:11:38 -0500

    xxx

--- bridge-data/pocconsumer/EmsHelper.cs
+++ bridge-data/pocconsumer/EmsHelper.cs
@@ -5,12 +5,13 @@ namespace POCConsumer;
 
 public static class EmsHelper
 {
+    private const string QueryStr = "query";
+ 
     static string GetUniqueQueryName()
     {
         string tempDir = Path.GetTempPath();
-        string baseFileName = $"query.{Guid.NewGuid().ToString()}";
+        string baseFileName = $"{QueryStr}.{Guid.NewGuid().ToString()}";
         string outFile = Path.Combine(tempDir, baseFileName);
-
         int counter = 0;
 
     retry:
@@ -22,5 +23,34 @@ public static class EmsHelper
         }
 
         return outFile;
+    }
+
+    static void MoveQuery(string tmpFileName, string pendingDir)
+    {
+        if (string.IsNullOrEmpty(tmpFileName) || string.IsNullOrEmpty(pendingDir))
+        {
+            return;
+        }
+
+        string fileName = Path.GetFileName(tmpFileName);
+        if (!fileName.StartsWith($"{QueryStr}."))
+        {
+            return;
+        }
+
+        // Ensure the pending directory exists
+        Directory.CreateDirectory(pendingDir);
+        string destFileName = Path.Combine(pendingDir, fileName);
+
+        int counter = 0;
+    retry:
+        if (File.Exists(destFileName))
+        {
+            counter++;
+            destFileName = Path.Combine(pendingDir, $"{fileName}.{counter}");
+            goto retry;
+        }
+
+        File.Move(tmpFileName, destFileName, true);
     }
 }
