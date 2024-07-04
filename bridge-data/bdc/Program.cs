a067bdefa83f582f49494fdfb6a6ce45d5c2c43e
Edwin Plauchu <j4nusx@yahoo.com>
2024-07-04 10:40:13 -0500

    main

---
+++ bridge-data/bdc/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(new CompactJsonFormatter())
+    .Enrich.FromLogContext()
+    .CreateBootstrapLogger();
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog((ctx, servs, conf) => conf
+        .ReadFrom.Configuration(ctx.Configuration)
+        .ReadFrom.Services(servs)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(new CompactJsonFormatter()));
+
+    var app = builder.Build();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly}");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
