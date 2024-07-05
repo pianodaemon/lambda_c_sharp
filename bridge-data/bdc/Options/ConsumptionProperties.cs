a39dab7192207483d7afe5ae149caffa3b147492
Edwin Plauchu <j4nusx@yahoo.com>
2024-07-05 18:55:49 -0500

    mxx

--- bridge-data/bdc/Options/ConsumptionProperties.cs
+++ bridge-data/bdc/Options/ConsumptionProperties.cs
@@ -6,6 +6,6 @@ public class ConsumptionProperties
 
     public string BucketName { get; init; } = "";
     public string QueueName { get; init; } = "";
-    public HashSet<string> DeferredQueryDirs { get; set; } = new HashSet<string>();
-    public HashSet<string> NonRestrictedDirs { get; set; } = new HashSet<string>();
+    public HashSet<string> DeferredQueryDirs { get; init; } = new HashSet<string>();
+    public HashSet<string> NonRestrictedDirs { get; init; } = new HashSet<string>();
 }
