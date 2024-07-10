4b067bd5f748834e8fe3bf1fe0f29dc2fadd3366
Edwin Plauchu <j4nusx@yahoo.com>
2024-07-10 09:02:04 -0500

    IFileMgmt

---
+++ bridge-data/bdc/Interfaces/IFileMgmt.cs
@@ -0,0 +1,7 @@
+namespace BridgeDataConsumer.Console.Interfaces;
+
+public interface IFileMgmt
+{
+    void MoveFileUnique(string sourcePath, string destinationPath);
+    void MoveQuery(string tmpFileName, string pendingDir);
+}
