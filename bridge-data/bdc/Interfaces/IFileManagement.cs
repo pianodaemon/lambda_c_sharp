a0ce419ff0a1f42a341481c8a5b5064962c053af
Edwin Plauchu <pianodaemon@gmail.com>
2024-08-15 18:59:46 -0600

    Update and rename IFileMgmt.cs to IFileManagement.cs

--- bridge-data/bdc/Interfaces/IFileManagement.cs
+++ bridge-data/bdc/Interfaces/IFileManagement.cs
@@ -1,8 +1,9 @@
 namespace BridgeDataConsumer.Console.Interfaces;
 
-public interface IFileMgmt
+public interface IFileManagement
 {
     void MoveFileUnique(string sourcePath, string destinationPath);
     void MoveQuery(string tmpFileName, string pendingDir);
     void MoveWithOverwrite(string tmpFileName, string pendingDir);
+    void ApplyStrategy(string sourcePath, string targetPath);
 }
